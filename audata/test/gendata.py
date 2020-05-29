aa39d49046a581f093355c384d0496fe2bfe18f1
Anthony Wertz <anthony.wertz@gmail.com>
2020-05-29 15:30:55 -0400

    Add runblocks.

--- audata/test/gendata.py
+++ audata/test/gendata.py
@@ -5,65 +5,65 @@ import os
 import datetime as dt
 import tzlocal
 
+if __name__ == '__main__':
+    def ensure(fn):
+        fn = 'patient123/{}'.format(fn)
+        os.makedirs(os.path.dirname(fn), exist_ok=True)
+        return fn
 
-def ensure(fn):
-    fn = 'patient123/{}'.format(fn)
-    os.makedirs(os.path.dirname(fn), exist_ok=True)
-    return fn
+    time_reference = dt.datetime.now(tzlocal.get_localzone())
+    P = 60*30 # s
 
-time_reference = dt.datetime.now(tzlocal.get_localzone())
-P = 60*30 # s
+    print('Making low-rate...')
+    Fs = 1./5 # Hz
+    N = int(Fs*P)
+    pd.DataFrame(
+        data={
+            'time': [time_reference + dt.timedelta(seconds=x/Fs) for x in range(N)],
+            'time2': [time_reference + dt.timedelta(seconds=x/Fs + 0.5) for x in range(N)],
+            'ints': list(range(N)),
+            'floats': [i*1.1 for i in list(range(N))],
+            'strings': [lorem.sentence() for x in range(N)],
+            'factor': pd.Series([['Cat', 'Dog', 'Liger'][x % 3] for x in range(N)], dtype='category')
+        }).to_csv(ensure('some_table.csv'), index=False)
 
-print('Making low-rate...')
-Fs = 1./5 # Hz
-N = int(Fs*P)
-pd.DataFrame(
-    data={
-        'time': [time_reference + dt.timedelta(seconds=x/Fs) for x in range(N)],
-        'time2': [time_reference + dt.timedelta(seconds=x/Fs + 0.5) for x in range(N)],
-        'ints': list(range(N)),
-        'floats': [i*1.1 for i in list(range(N))],
-        'strings': [lorem.sentence() for x in range(N)],
-        'factor': pd.Series([['Cat', 'Dog', 'Liger'][x % 3] for x in range(N)], dtype='category')
-    }).to_csv(ensure('some_table.csv'), index=False)
+    print('Making 500Hz ECG...')
+    Fs = 500 # Hz
+    N = int(Fs*P)
+    pd.DataFrame(
+        data={
+            'time': [x/Fs for x in range(N)],
+            'II': np.random.uniform(-2, 2, N),
+            'III': np.random.uniform(-2, 2, N),
+            'IV': np.random.uniform(-2, 2, N)
+        }).to_csv(ensure('waveform/ecg.csv'), index=False)
 
-print('Making 500Hz ECG...')
-Fs = 500 # Hz
-N = int(Fs*P)
-pd.DataFrame(
-    data={
-        'time': [x/Fs for x in range(N)],
-        'II': np.random.uniform(-2, 2, N),
-        'III': np.random.uniform(-2, 2, N),
-        'IV': np.random.uniform(-2, 2, N)
-    }).to_csv(ensure('waveform/ecg.csv'), index=False)
+    print('Making 250Hz waveform...')
+    Fs = 250 # Hz
+    N = int(Fs*P)
+    pd.DataFrame(
+        data={
+            'time': [x/Fs for x in range(N)],
+            'ART': np.random.uniform(50, 90, N),
+            'PAP': np.random.uniform(10, 15, N),
+            'CVP': np.random.uniform(0, 5, N)
+        }).to_csv(ensure('waveform/pressures.csv'), index=False)
 
-print('Making 250Hz waveform...')
-Fs = 250 # Hz
-N = int(Fs*P)
-pd.DataFrame(
-    data={
-        'time': [x/Fs for x in range(N)],
-        'ART': np.random.uniform(50, 90, N),
-        'PAP': np.random.uniform(10, 15, N),
-        'CVP': np.random.uniform(0, 5, N)
-    }).to_csv(ensure('waveform/pressures.csv'), index=False)
-
-print('Making vitals...')
-Fs = 1.5 # Hz
-N = int(Fs*P)
-pd.DataFrame(
-    data={
-        'time': [x/Fs for x in range(N)],
-        'Sys': np.random.uniform(80, 90, N)
-    }).to_csv(ensure('vitals/some_device/Sys.csv'), index=False)
-pd.DataFrame(
-    data={
-        'time': [x/Fs for x in range(N)],
-        'MAP': np.random.uniform(70, 80, N)
-    }).to_csv(ensure('vitals/some_device/MAP.csv'), index=False)
-pd.DataFrame(
-    data={
-        'time': [x/Fs for x in range(N)],
-        'Dia': np.random.uniform(50, 65, N)
-    }).to_csv(ensure('vitals/some_device/Dia.csv'), index=False)
+    print('Making vitals...')
+    Fs = 1.5 # Hz
+    N = int(Fs*P)
+    pd.DataFrame(
+        data={
+            'time': [x/Fs for x in range(N)],
+            'Sys': np.random.uniform(80, 90, N)
+        }).to_csv(ensure('vitals/some_device/Sys.csv'), index=False)
+    pd.DataFrame(
+        data={
+            'time': [x/Fs for x in range(N)],
+            'MAP': np.random.uniform(70, 80, N)
+        }).to_csv(ensure('vitals/some_device/MAP.csv'), index=False)
+    pd.DataFrame(
+        data={
+            'time': [x/Fs for x in range(N)],
+            'Dia': np.random.uniform(50, 65, N)
+        }).to_csv(ensure('vitals/some_device/Dia.csv'), index=False)
--- audata/test/readwrite.py
+++ audata/test/readwrite.py
@@ -7,47 +7,47 @@ import lorem
 
 from .. import AUFile
 
+if __name__ == '__main__':
+    f = AUFile.new('test.h5', overwrite=True)
 
-f = AUFile.new('test.h5', overwrite=True)
+    # Writing a matrix.
+    f['mymat'] = np.ones([100, 300])
 
-# Writing a matrix.
-f['mymat'] = np.ones([100, 300])
+    # Writing and appending a data frame.
+    N = 1000
+    df = pd.DataFrame(
+        data={
+            'time': [f.time_reference + dt.timedelta(seconds=x/250.0) for x in range(N)],
+            'time2': [f.time_reference + dt.timedelta(seconds=x/250.0 + 0.5) for x in range(N)],
+            'ints': list(range(N)),
+            'floats': [i*1.1 for i in list(range(N))],
+            'strings': [lorem.sentence() for x in range(N)],
+            'factor': pd.Series([['Cat', 'Dog', 'Liger'][x % 3] for x in range(N)], dtype='category')
+        }
+    )
+    f['test/mydf'] = df
+    f['test/mydf'].append(df)
 
-# Writing and appending a data frame.
-N = 1000
-df = pd.DataFrame(
-    data={
-        'time': [f.time_reference + dt.timedelta(seconds=x/250.0) for x in range(N)],
-        'time2': [f.time_reference + dt.timedelta(seconds=x/250.0 + 0.5) for x in range(N)],
-        'ints': list(range(N)),
-        'floats': [i*1.1 for i in list(range(N))],
-        'strings': [lorem.sentence() for x in range(N)],
-        'factor': pd.Series([['Cat', 'Dog', 'Liger'][x % 3] for x in range(N)], dtype='category')
-    }
-)
-f['test/mydf'] = df
-f['test/mydf'].append(df)
+    # Annotations - Not yet.
+    # f['mydf'].add_range('time', 'time2')
 
-# Annotations - Not yet.
-# f['mydf'].add_range('time', 'time2')
-
-# Read some data.
-print(f)
-print(f['.meta'])
-print(f.time_reference)
-print(f['test/mydf'])
-print(f['test']['mydf'][:])
-
-f.close()
-
-# Open the created file and do the same thing.
-print('Testing openeing file we just created:')
-with AUFile.open('test.h5') as f:
     # Read some data.
     print(f)
     print(f['.meta'])
     print(f.time_reference)
-    print(f['test']['mydf'])
-    print(f['test/mydf'][:])
-    print(f['test/mydf'][-3:])
-    print(f['test/mydf'][-3])
+    print(f['test/mydf'])
+    print(f['test']['mydf'][:])
+
+    f.close()
+
+    # Open the created file and do the same thing.
+    print('Testing openeing file we just created:')
+    with AUFile.open('test.h5') as f:
+        # Read some data.
+        print(f)
+        print(f['.meta'])
+        print(f.time_reference)
+        print(f['test']['mydf'])
+        print(f['test/mydf'][:])
+        print(f['test/mydf'][-3:])
+        print(f['test/mydf'][-3])
