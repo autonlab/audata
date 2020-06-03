048103bae2d9e2cc1a655ef56d468ca0648b13a9
Anthony Wertz <anthony.wertz@gmail.com>
2020-06-03 14:19:47 -0400

    Updated to v1.0.2

--- audata/__init__.py
+++ audata/__init__.py
@@ -1,9 +1,9 @@
 """Define version and import high-level objects."""
 __VERSION_MAJOR__ = '1'
 __VERSION_MINOR__ = '0'
-__VERSION_BUILD__ = '1'
+__VERSION_BUILD__ = '2'
 
-__DEBUG = True
+__DEBUG = False
 if __DEBUG:
     import datetime as dt
     __VERSION_TAGS__ = f"d{dt.datetime.now().strftime('%Y%m%d_%H%M')}"
