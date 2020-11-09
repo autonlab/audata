7dc28dfc51950244a1831323e4c1c3b72b9b16b6
Gus Welter <gwelter@andrew.cmu.edu>
2020-11-08 19:46:46 -0500

    Cleaned up wheel-build process (no longer requires numpy, imports version from main package without package dependency loop).

--- audata/__init__.py
+++ audata/__init__.py
@@ -8,10 +8,13 @@ if __DEBUG:
     import datetime as dt
     __VERSION_TAGS__ = f"d{dt.datetime.now().strftime('%Y%m%d_%H%M')}"
 else:
-    __VERSION_TAGS__ = 'rc1'
+    __VERSION_TAGS__ = ''
 
 __VERSION__ = f'{__VERSION_MAJOR__}.{__VERSION_MINOR__}.{__VERSION_BUILD__}{__VERSION_TAGS__}'
 
 __DATA_VERSION__ = '1.1'
 
-from audata.file import File
+try:
+    from audata.file import File
+except ImportError:
+    print("Unable to import audata.file.File. If this occurs during initial setup, you may ignore this.")
--- setup.py
+++ setup.py
@@ -2,31 +2,49 @@
 import os
 from setuptools import setup, find_packages
 
-import numpy as np
+#import numpy as np
 
 from audata import __VERSION__
 
 NAME = 'audata'
 VERSION = __VERSION__
 
-
 def read(filename):
     """Readlines of filename relative to setup file path."""
     return open(os.path.join(os.path.dirname(__file__), filename)).read()
 
-
 setup(
     name=NAME,
     version=VERSION,
-    description='A python package for reading and writing data for AUView.',
+    description='A python package for reading and writing data for in the audata file spec.',
     long_description=read('readme.md'),
     author='Anthony Wertz',
     author_email='awertz@cmu.edu',
     license='GNU LGPL 3',
-    entry_points={'console_scripts': ['csv2audata=audata.bin.csv2audata:main']},
+    entry_points={
+        'console_scripts': [
+            'csv2audata=audata.bin.csv2audata:main'
+        ]
+    },
     install_requires=[
-        'numpy', 'pandas', 'h5py', 'jsbeautifier', 'lorem', 'tzlocal', 'h5py'
+        'numpy',
+        'pandas',
+        'h5py==2.10.0',
+        'jsbeautifier',
+        'lorem',
+        'tzlocal',
+        'h5py'
     ],
     python_requires='>=3.7',
-    include_dirs=[np.get_include()],
-    packages=find_packages())
+    #include_dirs=[np.get_include()],
+    packages=find_packages(),
+    #setup_requires=['numpy'],
+    url='https://github.com/autonlab/audata',
+    classifiers=[
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Scientific/Engineering",
+    ],
+)
